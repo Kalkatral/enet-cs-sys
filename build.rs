965f9a38295164ac16de281ee76f1eaa575804a8
Toni Z <tonizana.dev@gmail.com>
2023-02-19 16:26:53 +0100

    Add automatic cmake build of ENet for linking

--- build.rs
+++ build.rs
@@ -1,10 +1,17 @@
+extern crate bindgen;
+extern crate cmake;
+
 use std::{env, path::PathBuf};
 
-extern crate bindgen;
+use cmake::Config;
 
 fn main() {
     println!("cargo:rerun-if-changed=wrapper.h");
 
+    let target = env::var("TARGET").unwrap();
+    let is_debug = env::var("DEBUG").unwrap() == "true";
+
+    // generate the bindings
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
@@ -12,8 +19,34 @@ fn main() {
         .generate()
         .expect("failed to generate bindings.");
 
+    // write the generated bindings to ${OUT_DIR}/bindings.rs
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("could not write bindings");
+
+    // build ENet-CS to a static library
+    let built = Config::new("vendor/ENet-CSharp/Source/Native")
+        .define("ENET_STATIC", "1")
+        .build_target("enet_static")
+        .build();
+
+    // add the newly compiled static library to the library search path of rustc
+    if target.contains("windows") {
+        if is_debug {
+            println!(
+                "cargo:rustc-link-search=native={}/build/Debug",
+                built.display()
+            );
+        } else {
+            println!(
+                "cargo:rustc-link-search=native={}/build/Release",
+                built.display()
+            );
+        }
+        println!("cargo:rustc-link-lib=dylib=winmm");
+    } else {
+        println!("cargo:rustc-link-search=native={}/build", built.display());
+    }
+    println!("cargo:rustc-link-lib=static=enet_static");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,35 @@ include! {concat!(env!("OUT_DIR"), "/bindings.rs")}
 
 #[cfg(test)]
 mod tests {
+    use std::{ffi::CString, mem::MaybeUninit, time::Duration};
+
+    use crate::*;
+
     #[test]
-    fn it_works() {
-        // yes
+    fn server_test() {
+        unsafe {
+            if enet_initialize() != 0 {
+                panic!("Enet could not be initialized.");
+            }
+
+            let address: MaybeUninit<ENetAddress> = MaybeUninit::uninit();
+            let mut address = address.assume_init();
+            // set the port to 8080
+            address.port = 8080;
+            // set the hostname to localhost 127.0.0.1; a CString must be used.
+            let address_hostname = CString::new("127.0.0.1").unwrap();
+            enet_address_set_hostname(&mut address, address_hostname.as_ptr());
+
+            // create host accepting 8 clients max, with 2 different channels, and automatically setting
+            // the in/out max bandwidth, and the buffer size by setting them to 0.
+            let host = enet_host_create(&address, 8, 2, 0, 0, 0);
+
+            // wait for a bit
+            std::thread::sleep(Duration::from_millis(1000));
+
+            // free the host and deinitialize ENet
+            enet_host_destroy(host);
+            enet_deinitialize();
+        }
     }
 }
